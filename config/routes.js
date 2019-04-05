271f7169b5e974d529842faab16361f2ceaf58fe
umbur <you@example.com>
2019-04-05 10:38:03 -0600

    created login endpoint/function using jwt to generate token

--- config/routes.js
+++ config/routes.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const bcrypt =  require('bcryptjs');
 const db = require('../database/dbConfig.js');
 
-const { authenticate } = require('../auth/authenticate');
+const { authenticate, generateToken } = require('../auth/authenticate');
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -44,6 +44,21 @@ function register(req, res) {
 
 function login(req, res) {
   // implement user login
+  let { username, password } = req.body;
+  db('users').where('username', username).first().then(user => {
+    console.log('user:', user)
+    if(user && bcrypt.compareSync(password, user.password)) {
+      const token = generateToken(user);
+      console.log('useraftertoken:', token)
+      res.status(200).json({ message: `Login completed for ${user.username}`, token })
+    } else {
+      res.status(401).json({ message: 'Invalid credentioals!' });
+    } 
+  }) .catch(err => {
+    console.log(err)
+    res.status(422).json({message: "Error, probably you've made a mistake"})
+  })
+
 }
 
 function getJokes(req, res) {
